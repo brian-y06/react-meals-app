2d2b9495b1097c8a618dbfd7f2c776afa5fb8b1f
Brian Ye <brian.ye2021@gmail.com>
2023-03-16 12:00:35 -0400

    added app-wide cart context

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./HeaderCardButton.module.css";
+import CartContext from '../../store/cart-context';
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext)
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{cartCtx.amount}</span>
     </button>
   );
 };
